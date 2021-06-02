826c4ff960c3213ff909d1570d4bb94a5f3cd8d8
Ender!11!!1!! <76421494+Enderfunguss@users.noreply.github.com>
2021-06-02 11:40:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(480,480);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function saveImg(){
+    save('mustachio man!.png');
+}
